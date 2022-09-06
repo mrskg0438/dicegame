c3b9ca9c777e8212e404d33f3ae6bc846af5cf62
Shwetkamal <gaudshwetkamal0438@gmail.com>
2022-09-07 00:46:59 +0530

    Added By Shwetkamal

--- myJava.java
+++ myJava.java
@@ -1,6 +1,7 @@
 class info{
 	public static void main(String[]args){
 		System.out.println("My Name Is Gaud Shwetkamal Kamlesh Kumar");
+		System.out.println("My Skills: Python(Basic), Java(Basic), Kali Linux, Metasploit, Ethical Hacking, HTML, CSS");
 		
 	}
 }
