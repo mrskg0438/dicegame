5fce0e45ef933c28f75be64d9ea5425138ce655d
Gaud Shwetkamal Kamlesh Kumar <108344772+mrskg0438@users.noreply.github.com>
2022-09-08 22:19:50 +0530

    Delete myJava.java

--- myJava.java
+++
@@ -1,7 +0,0 @@
-class info{
-	public static void main(String[]args){
-		System.out.println("My Name Is Gaud Shwetkamal Kamlesh Kumar");
-		System.out.println("My Skills: Python(Basic), Java(Basic), Kali Linux, Metasploit, Ethical Hacking, HTML, CSS");
-		
-	}
-}
